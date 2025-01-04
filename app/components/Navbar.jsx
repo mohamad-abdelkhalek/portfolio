62541bcb994984dde9a411818dc524a049f842b8
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2025-01-04 19:21:00 +0200

    Changed navbar font

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,8 +5,10 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4
-      flex item-center justify-between x-50">
+      <nav
+        className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4
+      flex item-center justify-between x-50"
+      >
         <a href="#top">
           <Image
             src={assets.logo}
@@ -16,29 +18,44 @@ const Navbar = () => {
           />
         </a>
 
-        <ul className="hidden md:flex items-center gap-6 lg:gap-8
-        rounded-full px-12 py-3">
+        <ul
+          className="hidden md:flex items-center gap-6 lg:gap-8
+        rounded-full px-12 py-3"
+        >
           <li>
-            <a href="#top">Home</a>
+            <a className="font-Ovo" href="#top">
+              Home
+            </a>
           </li>
           <li>
-            <a href="#about">About me</a>
+            <a className="font-Ovo" href="#about">
+              About me
+            </a>
           </li>
           <li>
-            <a href="#services">Services</a>
+            <a className="font-Ovo" href="#services">
+              Services
+            </a>
           </li>
           <li>
-            <a href="#work">My Work</a>
+            <a className="font-Ovo" href="#work">
+              My Work
+            </a>
           </li>
           <li>
-            <a href="#contact">Contact Me</a>
+            <a className="font-Ovo" href="#contact">
+              Contact Me
+            </a>
           </li>
         </ul>
 
         <div>
-          <a href="#contact" className="hidden lg:flex items-center gap-3 px-10
-          py-2.5 border border-gray-500 rounded-full ml-4">
-            Contact <Image src={assets.arrow_icon} alt="Logo" className="w-3"/>
+          <a
+            href="#contact"
+            className="hidden lg:flex items-center gap-3 px-10
+          py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo"
+          >
+            Contact <Image src={assets.arrow_icon} alt="Logo" className="w-3" />
           </a>
         </div>
       </nav>
