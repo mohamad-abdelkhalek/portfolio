521b5d6ec5f3e7e00afed0f4675b9583a8044992
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2025-01-04 19:26:05 +0200

    Modified navbar items styles & background

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,6 +5,9 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
+      <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
+        <Image src={assets.header_bg_color} alt="" className="w-full" />
+      </div>
       <nav
         className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4
       flex item-center justify-between x-50"
@@ -20,7 +23,7 @@ const Navbar = () => {
 
         <ul
           className="hidden md:flex items-center gap-6 lg:gap-8
-        rounded-full px-12 py-3"
+        rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50"
         >
           <li>
             <a className="font-Ovo" href="#top">
