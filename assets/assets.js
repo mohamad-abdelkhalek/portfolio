f9e1ea6638aef763db12a85376ac54dd0290d5de
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2025-01-05 20:29:43 +0200

    Updated technologies I use

--- assets/assets.js
+++ assets/assets.js
@@ -113,8 +113,8 @@ export const serviceData = [
 ]
 
 export const infoList = [
-    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML, CSS, JavaScript React Js, Next Js' },
-    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.Tech in Computer Science' },
+    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Technologies', description: 'Java, JavaScript, PHP, Python, TypeScript | MERN Stack | Bootstrap, Tailwind | Laravel | MariaDB, SQL, MySQL, PostgreSQL' },
+    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.Sc in Computer Science' },
     { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects' }
 ];
 
